d02f809331209d341b197b268692ff08e90aaa7d
Bo Chen <bochen@Bos-MacBook-Pro.local>
2019-05-14 19:59:31 -0700

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>home</div>
+    <router-link to="/signup">Sign Up</router-link>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+  .home {
+    font-size: 50px;
+  }
+</style>
---
+++ src/pages/signup/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Sign Up</div>
+</template>
+
+<script>
+export default {
+  name: 'Signup'
+}
+</script>
+
+<style scoped>
+
+</style>
